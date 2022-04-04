168dbab7da340687ce86d50fce259d777a5823d0
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-04-04 19:40:57 +0200

    add redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "utils/chakraTheme";
 import { UserProvider } from "@supabase/supabase-auth-helpers/react";
 import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "utils/reduxStore";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider supabaseClient={supabaseClient}>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </UserProvider>
+    <ReduxProvider store={store}>
+      <UserProvider supabaseClient={supabaseClient}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </UserProvider>
+    </ReduxProvider>
   );
 }
 
