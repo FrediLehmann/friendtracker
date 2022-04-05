2c88ec14d41b9f25c8bbc7e8d3892eeaf9d33412
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-04-05 21:47:47 +0200

    fetch profile

--- routes/profile/index.tsx
+++ routes/profile/index.tsx
@@ -3,9 +3,19 @@ import { NextPage } from "next";
 import Head from "next/head";
 import { useTranslation } from "next-i18next";
 import { EmailSection, GeneralSection, PhoneSection } from "./components";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserProfile, getUserProfile } from "store/user";
+import { useEffect } from "react";
 
 const Profile: NextPage = () => {
   const { t } = useTranslation(["profile", "common"]);
+  const dispatch = useDispatch();
+  const profile = useSelector(getUserProfile);
+
+  useEffect(() => {
+    if (profile.state === "init") dispatch(fetchUserProfile());
+  }, [profile, dispatch]);
+
   return (
     <>
       <Head>
--- store/user/index.tsx
+++ store/user/index.tsx
@@ -1,11 +1,48 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
 import { RootState } from "store";
+import { definitions } from "types/supabase";
+
+//#region actions
+
+export const fetchUserProfile = createAsyncThunk<
+  Promise<definitions["profiles"]>,
+  undefined
+>("user/profile", async () => {
+  let { data, error } = await supabaseClient
+    .from<definitions["profiles"]>("profiles")
+    .select("*")
+    .single();
+
+  if (error) throw error;
+  if (!data) throw new Error("No data");
+
+  return data;
+});
+
+//#endregion
 
 //#region state
 
+type UserState = {
+  loggedIn: boolean;
+  loadingLoginStatus: boolean;
+  email: string;
+  profile: {
+    state: "init" | "loading" | "loaded" | "error";
+  } & definitions["profiles"];
+};
+
+const initialState: UserState = {
+  loggedIn: false,
+  loadingLoginStatus: true,
+  email: "",
+  profile: { state: "init", owner: "" },
+};
+
 const userSlice = createSlice({
   name: "user",
-  initialState: { loggedIn: false, loadingLoginStatus: true, email: "" },
+  initialState,
   reducers: {
     setUserLoggedIn: (state, { payload }: PayloadAction<boolean>) => {
       state.loggedIn = payload;
@@ -16,6 +53,19 @@ const userSlice = createSlice({
     setMainEmail: (state, { payload }: PayloadAction<string>) => {
       state.email = payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserProfile.pending, (state) => {
+      state.profile.state = "loading";
+    });
+
+    builder.addCase(fetchUserProfile.rejected, (state) => {
+      state.profile.state = "error";
+    });
+
+    builder.addCase(fetchUserProfile.fulfilled, (state, { payload }) => {
+      state.profile = { ...state.profile, state: "loaded", ...payload };
+    });
   },
 });
 
@@ -34,5 +84,7 @@ export const getUserLoginLoadingState = (state: RootState) =>
   state.user.loadingLoginStatus;
 
 export const getUserEmail = (state: RootState) => state.user.email;
+
+export const getUserProfile = (state: RootState) => state.user.profile;
 
 //#endregion
