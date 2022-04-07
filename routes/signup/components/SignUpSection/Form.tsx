5ab3d0abd25c0d63def0ab325bf289acdaf3d2f9
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-04-07 16:44:24 +0200

    button fix

--- routes/signup/components/SignUpSection/Form.tsx
+++ routes/signup/components/SignUpSection/Form.tsx
@@ -77,6 +77,7 @@ export default function Form({
             </CheckBoxField>
             <Button
               colorScheme="blue"
+              w="full"
               mt="4"
               type="submit"
               isLoading={isSubmitting}
