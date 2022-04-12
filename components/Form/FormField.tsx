4b5167dfe6e0da114b76ea5cd35ac90aa72fdd08
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-04-12 13:45:35 +0200

    input addons

--- components/Form/FormField.tsx
+++ components/Form/FormField.tsx
@@ -5,28 +5,35 @@ import {
   FormHelperText,
   FormLabel,
   Input,
+  InputGroup,
+  InputLeftAddon,
+  InputRightAddon,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 
 function FormField({
   name,
   label,
+  type,
   placeholder,
   helperText,
   helperPosition,
   isRequired = false,
-  type,
   hideLabel = false,
+  leftAddon,
+  rightAddon,
   ...props
 }: {
   name: string;
   label: string;
+  type?: string;
   placeholder?: string;
   helperText?: string;
   helperPosition?: "before" | "after";
-  type?: string;
-  hideLabel?: boolean;
   isRequired?: boolean;
+  hideLabel?: boolean;
+  leftAddon?: React.ReactNode;
+  rightAddon?: React.ReactNode;
 }) {
   const [field, meta] = useField({ name, ...props });
 
@@ -43,13 +50,17 @@ function FormField({
           {helperText}
         </FormHelperText>
       )}
-      <Input
-        id={name}
-        type={type}
-        placeholder={placeholder}
-        {...field}
-        {...props}
-      />
+      <InputGroup>
+        {leftAddon && <InputLeftAddon px="0">{leftAddon}</InputLeftAddon>}
+        <Input
+          id={name}
+          type={type}
+          placeholder={placeholder}
+          {...field}
+          {...props}
+        />
+        {rightAddon && <InputRightAddon px="0">{rightAddon}</InputRightAddon>}
+      </InputGroup>
       {helperText && helperPosition === "after" && (
         <FormHelperText fontSize={["sm", "md"]} mb="3">
           {helperText}
