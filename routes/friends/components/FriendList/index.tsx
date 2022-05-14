5a416076d1fa7955a31f9e0543c30c944f08a080
FrediLehmann <frederic.lehmann@bluewin.ch>
2022-05-14 16:48:55 +0200

    show friend list

--- routes/friends/components/FriendList/index.tsx
+++ routes/friends/components/FriendList/index.tsx
@@ -1,27 +1,66 @@
 import { Flex, Text } from "@chakra-ui/react";
+import { supabaseClient } from "@supabase/supabase-auth-helpers/nextjs";
+import { useUser } from "@supabase/supabase-auth-helpers/react";
 import { useTranslation } from "next-i18next";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getUserProfile } from "store/user";
 import { Friend } from "./components";
+
+interface FriendList {
+  owner: string;
+  user_name?: string;
+  avatar_url?: string;
+  profile_hash: string;
+  friends: {
+    request_status: string;
+    initiator: string;
+  }[];
+}
 
 export default function FriendList() {
   const { t } = useTranslation("friends");
+
+  const { user } = useUser();
+
+  const { profile_hash } = useSelector(getUserProfile);
+
+  const [friends, setFriends] = useState<FriendList[]>();
+
+  useEffect(() => {
+    const getFriends = async () => {
+      const { data, error } = await supabaseClient
+        .from("profiles")
+        .select(
+          "owner, user_name, avatar_url, profile_hash, friends!friends_friend_fkey!inner(request_status, initiator)"
+        )
+        .eq("friends.initiator", profile_hash);
+
+      if (error) throw error;
+
+      setFriends(data);
+    };
+
+    user && profile_hash && getFriends();
+  }, [user, profile_hash]);
+
   return (
     <>
       <Text textAlign="end" color="gray.500" fontSize="sm" mb="2">
-        {t("friendList.count", { count: 15 })}
+        {t("friendList.count", { count: friends?.length || 0 })}
       </Text>
       <Flex direction="column" gap="3">
-        <Friend
-          name="Hannes Kaufmann"
-          url="hanneskaufmann"
-          lastSignIn="24.03.2022"
-        />
-        <Friend name="Wendy Nguyen" url="wendynguyen" lastSignIn="12.02.2022" />
-        <Friend name="Markus Trachsel" url="markustrachsel" isPending />
-        <Friend
-          name="Frederic Lehmann"
-          url="fredericlehmann"
-          lastSignIn="03.03.2022"
-        />
+        {friends &&
+          friends.length > 0 &&
+          friends.map((friend) => (
+            <Friend
+              key={friend.profile_hash}
+              name={friend.user_name || ""}
+              url={friend.profile_hash}
+              isPending={friend.friends[0]?.request_status === "pending"}
+              lastSignIn="24.03.2022"
+            />
+          ))}
       </Flex>
     </>
   );
